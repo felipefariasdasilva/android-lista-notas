9d3b139636702b93e6514421a1c189d06cfd6e68
felipefariasdasilva <feliipefarias@outlook.com>
2020-05-16 10:25:05 -0300

    changed list view to recycler view

--- app/src/main/java/com/felipe/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/felipe/ceep/ui/activity/ListaNotasActivity.java
@@ -1,10 +1,17 @@
 package com.felipe.ceep.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.felipe.ceep.R;
+import com.felipe.ceep.dao.NotaDAO;
+import com.felipe.ceep.model.Nota;
+import com.felipe.ceep.ui.recyclerView.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +19,24 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        List<Nota> todasNotas = notasDeExemplo();
+        configurarRecyclerView(todasNotas);
+    }
+
+    private List<Nota> notasDeExemplo() {
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira Nota", "Descrição Pequena"));
+        dao.insere(new Nota("Segunda Nota", "Segunda Descrição é Bem Maior Que a Primeira Nota"));
+        return dao.todos();
+    }
+
+    private void configurarRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        configurarAdapter(todasNotas, listaNotas);
+    }
+
+    private void configurarAdapter(List<Nota> todasNotas, RecyclerView listaNotas) {
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 }
---
+++ app/src/main/java/com/felipe/ceep/ui/recyclerView/adapter/ListaNotasAdapter.java
@@ -0,0 +1,65 @@
+package com.felipe.ceep.ui.recyclerView.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.felipe.ceep.R;
+import com.felipe.ceep.model.Nota;
+
+import java.util.List;
+
+public class ListaNotasAdapter extends RecyclerView.Adapter<ListaNotasAdapter.NotaViewHolder> {
+
+    private final List<Nota> notas;
+    private final Context context;
+
+    public ListaNotasAdapter(Context context, List<Nota> notas) {
+        this.context = context;
+        this.notas = notas;
+    }
+
+    @NonNull
+    @Override
+    public ListaNotasAdapter.NotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
+        return new NotaViewHolder(viewCriada);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListaNotasAdapter.NotaViewHolder holder, int position) {
+        Nota nota = notas.get(position);
+        holder.vincular(nota);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notas.size();
+    }
+
+    class NotaViewHolder extends RecyclerView.ViewHolder{
+
+        private final TextView titulo;
+        private final TextView descricao;
+
+        public NotaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titulo = itemView.findViewById(R.id.item_nota_titulo);
+            descricao = itemView.findViewById(R.id.item_nota_descricao);
+        }
+
+        public void vincular(Nota nota){
+            preencherCampos(nota);
+        }
+
+        private void preencherCampos(Nota nota) {
+            titulo.setText(nota.getTitulo());
+            descricao.setText(nota.getDescricao());
+        }
+    }
+}
